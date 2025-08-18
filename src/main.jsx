f9826c3439caec23db6f6bff12ab96d13205f8f2
Amed-Saad <ahmed.saad.bayoumi.farag@gmail.com>
2025-08-18 14:53:18 +0300

    Edit_7

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
-import AuthContextProvider from "./Context/authContext.jsx";
+import AuthContextProvider from "./Context/AuthContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
