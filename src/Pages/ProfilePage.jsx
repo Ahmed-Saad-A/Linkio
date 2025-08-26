9c803bfe7b762330c83340a74398b6769db30fa9
Amed-Saad <ahmed.saad.bayoumi.farag@gmail.com>
2025-08-26 21:36:30 +0300

    Edit_22

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -5,7 +5,7 @@ import CreatePost from "../Components/Post/CreatePost";
 import { AuthContext } from "../Context/AuthContextProvider";
 import { getUserPostsApi } from "../Services/UserServices";
 import PostSkeleton from "../Components/Post/PostSkeleton";
-import Index from '../Components/Post';
+import Index from '../Components/Post/index.jsx';
 
 
 const ProfilePage = () => {
